cdaddd32d5e91bae66397f8daa7d853c1ec4a24a
Jay Kapoor <jkap86@gmail.com>
2025-01-03 13:33:30 -0500

    -

--- src/app/api/leagues/helpers/updateLeagues.ts
+++ src/app/api/leagues/helpers/updateLeagues.ts
@@ -153,7 +153,10 @@ export const updateLeagues = async (
               });
             });
 
-          if (week) {
+          if (
+            week &&
+            rosters.data.find((r: SleeperRoster) => r.players?.length > 0)
+          ) {
             const trades_current = await getTrades(
               leagueToUpdate,
               season === league.data.season ? week : "1",
